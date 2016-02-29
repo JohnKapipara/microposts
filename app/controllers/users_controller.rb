b8947377f9e695c1b256fcbdd3f2cf647350102f
johnkapipara1 <cgphk621@ybb.ne.jp>
2016-02-29 16:38:32 +0000

    add user-profile report no.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def show
     @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "更新しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -20,6 +33,11 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :from, :birthday)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
